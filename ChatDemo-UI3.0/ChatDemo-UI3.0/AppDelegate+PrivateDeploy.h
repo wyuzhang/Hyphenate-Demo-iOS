78a33be269b2e0a1efaf67fc107a626e30bd706f
wyuzhang <sxqxdong@sina.com>
2017-06-01 19:39:00 +0800

    add

---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/AppDelegate+PrivateDeploy.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+PrivateDeploy.h
+//  ChatDemo-UI3.0
+//
+//  Created by WYZ on 2017/6/1.
+//  Copyright © 2017年 easemob. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate (PrivateDeploy)
+
+@end
---
+++ ChatDemo-UI3.0/ChatDemo-UI3.0/AppDelegate+PrivateDeploy.m
@@ -0,0 +1,13 @@
+//
+//  AppDelegate+PrivateDeploy.m
+//  ChatDemo-UI3.0
+//
+//  Created by WYZ on 2017/6/1.
+//  Copyright © 2017年 easemob. All rights reserved.
+//
+
+#import "AppDelegate+PrivateDeploy.h"
+
+@implementation AppDelegate (PrivateDeploy)
+
+@end
